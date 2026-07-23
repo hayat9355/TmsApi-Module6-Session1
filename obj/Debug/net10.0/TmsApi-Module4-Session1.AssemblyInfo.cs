b27b5cdc791dc36a76afdd7568515222482254ab
hayat9355 <hayatyimam209@gmail.com>
2026-07-23 16:06:36 +0300

    M6-Session-1

--- obj/Debug/net10.0/TmsApi-Module4-Session1.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi-Module4-Session1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi-Module4-Session1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e3f521c264bb5a3457d56e57682d67d3b74cd0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe69311df01785ca19cd4340cb5dc9377d7e0b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi-Module4-Session1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi-Module4-Session1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
